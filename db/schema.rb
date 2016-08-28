0b04039c2327c442f303c69088039f8d6c5893aa
Webgoal <webgoal@iMac-de-Flavio.local>
2016-08-28 11:30:59 -0300

    Fix bugssss

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160828001000) do
+ActiveRecord::Schema.define(version: 20160828125505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20160828001000) do
     t.string "url"
     t.string "user"
     t.string "password"
+  end
+
+  create_table "senhas", force: :cascade do |t|
+    t.string   "Nome"
+    t.string   "Url"
+    t.string   "Senha"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tests", force: :cascade do |t|
